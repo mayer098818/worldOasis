b331ed8a7688515398262f602da9bc925e4d8f3f
May <yxl4816@163.com>
2026-02-05 17:29:31 +0800

    loginv3

--- src/features/authentication/Signup.tsx
+++ src/features/authentication/Signup.tsx
@@ -3,12 +3,30 @@ import CabinForm from "../cabins/CabinForm"
 import Button from "../../ui/Button"
 import styled from "styled-components"
 import Form from "../../ui/Form"
+const LoginLayout = styled.main`
+  min-height: 100vh;
+  display: grid;
+  grid-template-columns: 48rem;
+  align-content: center;
+  justify-content: center;
+  gap: 3.2rem;
+  background-image:url('../../public/backgroundimage.jpeg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-blend-mode: overlay;
+`;
 
-const Box = styled.div`
-display:flex;
-justify-content:center;
-margin-top:2.4rem;
-`
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 2.4rem;
+  
+  & > * {
+    width: 100%;
+  }
+`;
 const cabinConfig: any[] = [
     {
         id: 'fullName',
@@ -30,6 +48,12 @@ const cabinConfig: any[] = [
         type: 'input',
         rules: { required: 'Password is required' }
     },
+    {
+        id: 'passwordConfirm',
+        label: 'Password Confirm',
+        type: 'input',
+        rules: { required: 'Password Confirm is required' }
+    },
 
 ]
 const Signup = () => {
@@ -37,14 +61,24 @@ const Signup = () => {
     const onSubmit = (data) => {
         console.log(data, 'onSubmit')
     }
+    // return (
+    //     <Form type="regular" onSubmit={handleSubmit(onSubmit)}>
+    //         <CabinForm cabinConfig={cabinConfig} control={control} errors={errors} />
+    //         <Box>
+    //             {/* <Button>Cancel</Button> */}
+    //             <Button variation='primary'>Signup</Button>
+    //         </Box>
+    //     </Form>
+    // )
     return (
-        <Form type="regular" onSubmit={handleSubmit(onSubmit)}>
-            <CabinForm cabinConfig={cabinConfig} control={control} errors={errors} />
-            <Box>
-                {/* <Button>Cancel</Button> */}
-                <Button variation='primary'>Signup</Button>
-            </Box>
-        </Form>
+        <LoginLayout>
+            <Form type="login" onSubmit={handleSubmit(onSubmit)}>
+                <CabinForm type="vertical" cabinConfig={cabinConfig} errors={errors} control={control}></CabinForm>
+                <ButtonWrapper>
+                    <Button variation="primary" >Create an account</Button>
+                </ButtonWrapper>
+            </Form>
+        </LoginLayout>
     )
 }
 export default Signup
